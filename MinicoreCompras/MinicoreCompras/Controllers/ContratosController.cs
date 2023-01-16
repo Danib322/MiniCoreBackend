a0751a32793a2cc47419f8b5dd750811007c99c5
Daniel Bustos <daniel.bustos.velez@udla.edu.ec>
2023-01-15 21:34:55 -0500

    Proyecto contratos Backend

---
+++ MinicoreCompras/MinicoreCompras/Controllers/ContratosController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinicoreCompras.Models;
+
+namespace MinicoreCompras.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContratosController : ControllerBase
+    {
+        private readonly MiniCoreComprasContext _context;
+
+        public ContratosController(MiniCoreComprasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Contratos
+        [HttpGet(Name = "~/GetContratos")]
+        public async Task<ActionResult<IEnumerable<Contrato>>> GetContratos()
+        {
+            return await _context.Contratos.ToListAsync();
+        }
+
+        // GET: api/Contratos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contrato>> GetContrato(int id)
+        {
+            var contrato = await _context.Contratos.FindAsync(id);
+
+            if (contrato == null)
+            {
+                return NotFound();
+            }
+
+            return contrato;
+        }
+
+        // PUT: api/Contratos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutContrato(int id, Contrato contrato)
+        {
+            if (id != contrato.IdContrato)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(contrato).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ContratoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Contratos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Contrato>> PostContrato(Contrato contrato)
+        {
+            _context.Contratos.Add(contrato);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetContrato", new { id = contrato.IdContrato }, contrato);
+        }
+
+        // DELETE: api/Contratos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteContrato(int id)
+        {
+            var contrato = await _context.Contratos.FindAsync(id);
+            if (contrato == null)
+            {
+                return NotFound();
+            }
+
+            _context.Contratos.Remove(contrato);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        
+
+        private bool ContratoExists(int id)
+        {
+            return _context.Contratos.Any(e => e.IdContrato == id);
+        }
+    }
+}
